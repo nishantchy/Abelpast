1670e510587157593e8f6bf120ad40e1178423a0
nishantchy <nishantchy1234@gmail.com>
2024-05-22 16:37:48 +0545

    second

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const numbers = document.querySelectorAll(".number");
+        numbers.forEach((number) => {
+          animateNumber(number);
+        });
+        observer.disconnect();
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+  const target = document.querySelector("#stats");
+  observer.observe(target);
+
+  function animateNumber(element) {
+    const max = +element.getAttribute("data-max");
+    let current = 0;
+    const increment = max / 100;
+    const interval = setInterval(() => {
+      current += increment;
+      if (current >= max) {
+        element.textContent = max;
+        clearInterval(interval);
+      } else {
+        element.textContent = Math.ceil(current);
+      }
+    }, 20);
+  }
+});
